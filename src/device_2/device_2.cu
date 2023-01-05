1145d3d8bfcd01cf51485edfee39ad9450b68d80
DESKTOP-OQE2VNJ\trank <60394372+tristran2k1@users.noreply.github.com>
2023-01-05 15:43:16 +0700

    update v2

--- src/device_2/device_2.cu
+++ src/device_2/device_2.cu
@@ -34,7 +34,7 @@ __global__ void blurImg_kernel_v2(int *inPixels, int width, int height, int *out
     }
 }
 
-__global__ void calcEnergyMap_kernel_v2(int *inPixels, int width, int height, int *outPixels) //DONE
+__global__ void calcEnergyMap_kernel_v2(int *inPixels, int width, int height, int *outPixels)
 {
     extern __shared__ int s_inPixels[];
     int sDim_x = blockDim.x + SOBEL_KERNEL_SIZE - 1;
@@ -106,6 +106,7 @@ void removeSingleSeam(uchar3 *inPixels, int width, int height, int seam_order, u
 
     size_t pixelsSize_3channels = width * height * sizeof(uchar3);
     size_t pixelsSize_1channels = width * height * sizeof(int);
+    size_t smem_size2alloc;
 
     CHECK(cudaMalloc(&d_src, pixelsSize_3channels));
     CHECK(cudaMemcpy(d_src, src, pixelsSize_3channels, cudaMemcpyHostToDevice));
@@ -119,14 +120,15 @@ void removeSingleSeam(uchar3 *inPixels, int width, int height, int seam_order, u
     // 2. Blur gray img
     CHECK(cudaMalloc(&d_src_blur, pixelsSize_1channels));
     dim3 gridSize_blur((width - 1) / blockSize.x + 1, (height - 1) / blockSize.y + 1);
+    smem_size2alloc = (blockSize.x + BLUR_KERNEL_SIZE - 1) * (blockSize.y + BLUR_KERNEL_SIZE - 1) * sizeof(int);
 
-    blurImg_kernel_v2<<<gridSize_blur, blockSize>>>(d_src_gray, width, height, d_src_blur);
+    blurImg_kernel_v2<<<gridSize_blur, blockSize, smem_size2alloc>>>(d_src_gray, width, height, d_src_blur);
 
     // 3. Calc Energies
     CHECK(cudaMalloc(&d_energies, pixelsSize_1channels));
+    smem_size2alloc = (blockSize.x + SOBEL_KERNEL_SIZE - 1) * (blockSize.y + SOBEL_KERNEL_SIZE - 1) * sizeof(int);
 
-    calcEnergyMap_kernel_v2<<<gridSize_blur, blockSize>>>(d_src_blur, width, height, d_energies);
-
+    calcEnergyMap_kernel_v2<<<gridSize_blur, blockSize, smem_size2alloc>>>(d_src_blur, width, height, d_energies);
     int *energy = (int *)malloc(pixelsSize_1channels);
     CHECK(cudaMemcpy(energy, d_energies, pixelsSize_1channels, cudaMemcpyDeviceToHost));
 
@@ -146,7 +148,6 @@ void removeSingleSeam(uchar3 *inPixels, int width, int height, int seam_order, u
     {
         min_path[i] = min_array[(i - 1) * width + min_path[i - 1]];
     }
-
     // 6. Remove scene
     uchar3 *d_output;
     int *d_min_path;
